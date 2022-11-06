e174b62c8bead677278aa77a41b6a06b3c487bef
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-11-06 09:35:50 -0800

    coded for products and store some function added for make title short then maped on productData then in product component show them in UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
+// Component
+import Store from './component/Store';
+
 // context
 import ProductContextProvider from './context/ProductContextProvider';
 
 function App() {
   return (
     <ProductContextProvider>
-      <p>hello</p>
+      <Store />
     </ProductContextProvider>
   );
 }
