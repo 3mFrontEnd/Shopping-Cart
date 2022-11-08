3823e958db7c643b30e02ab470d2d43364683d87
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-11-07 19:19:51 -0800

    codes for button are done onclick event applied and works good

--- src/component/shared/Product.js
+++ src/component/shared/Product.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 
 // functions
-import { shorten } from '../../helpers/function';
+import { shorten, isInCart, quantityCount } from '../../helpers/function';
+
+// context
+import { CartContext } from '../../context/CartContextProvider';
+
+// icons
+import trashIcon from "../../assets/icons/trash.svg";
 
 const Product = ({productData}) => {
+
+    const {state, dispatch} = useContext(CartContext);
     return (
         <div>
             <img src={productData.image} alt="product pic" style={{width: "200px"}} />
@@ -13,7 +21,13 @@ const Product = ({productData}) => {
             <div>
                 <Link to={`/products/${productData.id}`}>Details</Link>
                 <div>
-                    <button>Add to Cart</button>
+                    {quantityCount(state, productData.id) > 1 && <button onClick={()=> dispatch({type:"DECREASE", payload: productData})}>-</button>}
+                    {quantityCount(state, productData.id) === 1 && <button onClick={()=> dispatch({type:"REMOVE_ITEM", payload: productData})}><img src={trashIcon} alt="trash icon" /></button>}
+                    {
+                        isInCart(state, productData.id) ?
+                        <button onClick={()=> dispatch({type:"INCREASE", payload: productData})}>+</button> :
+                        <button onClick={()=> dispatch({type:"ADD_ITEM", payload: productData})}>Add to Cart</button>
+                    }
                 </div>
             </div>
         </div>
